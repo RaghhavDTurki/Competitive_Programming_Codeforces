cce5fdad3b973f76caa2b4381220131aded2e404
raghhav <raghhavdturki@gmail.com>
2021-02-20 16:33:13 +0530

    Updating the repository

---
+++ Codeforces_Round702_Div3/C_Sum_of_Cubes.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 20 12:21:13 2021
+
+@author: raghhav
+"""
+t = int(input()) 
+f = []
+for _ in range(t):
+    n = int(input())
+    cubes = {}
+    for i in range(1,int(n**(1./3.))+8):
+        cubes[i**3] = 1
+    for i in range(1,len(cubes)): 
+        a = i**3
+        b = n - a
+        if b in cubes.keys():
+            ans = "YES"
+            break
+        else:
+            ans = "NO"
+    f.append(ans)
+for k in f:
+    print(k)
